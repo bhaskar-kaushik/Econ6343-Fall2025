7d7333d4eabbbe366fd5e47a526ebc05e4f4b2dc
bhaskar-kaushik <bhaskar.kaushik-1@ou.edu>
2025-06-08 04:54:47 +0530

    Initial setup for Econ 6343

---
+++ startup.jl
@@ -0,0 +1,13 @@
+# Econ 6343 Startup Script
+using DataFrames
+using CSV
+using Plots
+using Statistics
+using LinearAlgebra
+using Distributions
+using Optim
+using Random
+
+println("Econ 6343 environment loaded!")
+println("Working directory: ", pwd())
+
